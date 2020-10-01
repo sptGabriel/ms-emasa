d8b603177e658d0192626cff6c2248356aa82dba
unknown <you@example.com>
2020-10-01 00:46:15 -0300

    fix any

--- packages/ms-employee/src/modules/locations/infrastucture/location.repository.ts
+++ packages/ms-employee/src/modules/locations/infrastucture/location.repository.ts
@@ -1,25 +1,12 @@
 import { ILocationRepository } from './contracts/locationRepository';
 import { LocationModel } from './location.model';
 import { Location } from '../domain/location.entity';
-import { Model } from 'objection';
+import { Model, ModelObject } from 'objection';
+import { BaseRepository } from 'shared/core/utils/entityRepository';
+import { container } from 'tsyringe';
 
-export class LocationRepository<Location>
-  implements ILocationRepository<Location> {
-  private model: LocationModel;
-  constructor() {}
-  public find = async (Data: Object): Promise<Location> => {
-    throw new Error('Method not implemented.');
-  };
-  public findAll(): Promise<Location> {
-    throw new Error('Method not implemented.');
-  }
-  public create(Data: Object): Promise<Location> {
-    throw new Error('Method not implemented.');
-  }
-  public delete(Data: Object): Promise<Location> {
-    throw new Error('Method not implemented.');
-  }
-  public update(data: Object): Promise<Location> {
-    throw new Error('Method not implemented.');
+export class LocationRepository extends BaseRepository<Location> {
+  constructor() {
+    super(container.resolve('knex'), LocationModel);
   }
 }

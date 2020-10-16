3975845fe6c881677bab9e0632e090050895cb3f
unknown <you@example.com>
2020-10-16 02:05:28 -0300

    fix:any

--- packages/ms-employee/src/shared/core/utils/entityRepository.ts
+++ packages/ms-employee/src/shared/core/utils/entityRepository.ts
@@ -1,16 +1,16 @@
 import { Model, ModelClass } from 'objection';
 import Knex from 'knex';
 import { IMapper } from '../infra/mapper';
-export interface IRead<T> {
-  find(id: string): Promise<T | undefined>;
-  findAll(): Promise<T[]>;
+export interface IRead {
+  find(id: string): Promise<any | undefined>;
+  findAll(): Promise<any[]>;
 }
-export interface IWrite<T> {
-  create(item: Object): Promise<T>;
-  update(id: string, item: any): Promise<T>;
-  delete(id: string): Promise<T>;
+export interface IWrite {
+  create(item: Object): Promise<any>;
+  update(id: string, item: any): Promise<any>;
+  delete(id: string): Promise<any>;
 }
-export abstract class BaseRepository<T> implements IWrite<T>, IRead<T> {
+export abstract class BaseRepository implements IWrite, IRead {
   protected db: Knex;
   protected tableName: string;
   protected transactionProvider: () => Promise<Knex.Transaction<any, any>>;
@@ -19,11 +19,11 @@ export abstract class BaseRepository<T> implements IWrite<T>, IRead<T> {
     this.tableName = table;
     this.transactionProvider = this.db.transactionProvider();
   }
-  abstract find(id: string): Promise<T | undefined>;
-  abstract findAll(): Promise<T[]>;
-  abstract create(item: Object): Promise<T>;
-  abstract update(id: string, item: T): Promise<T>;
-  abstract delete(id: string): Promise<T>;
+  abstract find(id: string): Promise<any | undefined>;
+  abstract findAll(): Promise<any[]>;
+  abstract create(item: Object): Promise<any>;
+  abstract update(id: string, item: any): Promise<any>;
+  abstract delete(id: string): Promise<any>;
   public get Connection() {
     if (!this.db) throw new Error('Knex connection instance not implemented.');
     return this.db;
